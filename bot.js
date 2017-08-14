825227a17808617e9a92687b819f42d12a4ff3ce
The-Complex <willculhane3@gmail.com>
2017-08-13 19:14:30 -0600

    Update log in case of isolated $sweep message

--- bot.js
+++ bot.js
@@ -10,156 +10,160 @@ const token = config.token;
 
 
 bot.on('ready', () => {
-  console.log('0x526561647921');
+	console.log('0x526561647921');
 });
 
 bot.on('disconnect', event => {
-  console.log('!Disconnected: ' + event.reason + ' (' + event.code + ')!');
+	console.log('!Disconnected: ' + event.reason + ' (' + event.code + ')!');
 });
 
 bot.on('message', message => {
-  console.log('\n////NEW MESSAGE////');
-  console.log('Time: ' + Date());
-  console.log('From: ' + message.author.username);
-  console.log('Message: ' + message.content);
-  console.log('Channel ID: ' + message.channel.id)
-  console.log('Channel Name: ' + message.channel.name)
-  const messageSplit = message.content.split(' ');
-  if (message.content.toLowerCase().includes('phil')) message.delete();
-  console.log('Output of censor: ' + JSON.stringify(censor.getCategoryCounts(message.content)));
-  censorLoop:
-  for (i = 0; i < messageSplit.length; i++) {
-    let simpleWord = messageSplit[i].replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").replace(/\s{2,}/g," ").toLowerCase().replace(/[^\w\s]|(.)(?=\1)/gi, "");
-    console.log('Checking: '+ messageSplit[i]);
-    console.log(censor.hasWord(simpleWord));
+	console.log('\n////NEW MESSAGE////');
+	console.log('Time: ' + Date());
+	console.log('From: ' + message.author.username);
+	console.log('Message: ' + message.content);
+	console.log('Channel ID: ' + message.channel.id)
+	console.log('Channel Name: ' + message.channel.name)
+	const messageSplit = message.content.split(' ');
+	if (message.content.toLowerCase().includes('phil')) message.delete();
+	console.log('Output of censor: ' + JSON.stringify(censor.getCategoryCounts(message.content)));
+	censorLoop:
+	for (i = 0; i < messageSplit.length; i++) {
+		let simpleWord = messageSplit[i].replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").replace(/\s{2,}/g," ").toLowerCase().replace(/[^\w\s]|(.)(?=\1)/gi, "");
+		console.log('Checking: '+ messageSplit[i]);
+		console.log(censor.hasWord(simpleWord));
 
-    if (censor.hasWord(simpleWord)) {
-      console.log('Profanity present in message from: ' + message.author.username);
-      message.reply({'content': '🚫 ¡LANGUAGE CENSORSHIP! 🚫', "embed": {
-          "title": "¡LANGUAGE CENSORSHIP!",
-          "description": '"' + messageSplit[i].replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").replace(/\s{2,}/g," ") + '" is a bad word. Don\'t use it. >:(',
-          "color": 0xff0000,
-          "footer": {
-            "text": "Glory to LEARAX57H"
-          },
-          "image": {
-            "url": "https://getadblock.com/images/adblock_logo_stripe_test.png"
-          }
-        }
-      });
-      break censorLoop;
-    };
-  };
+		if (censor.hasWord(simpleWord)) {
+			console.log('Profanity present in message from: ' + message.author.username);
+			message.reply({'content': '🚫 ¡LANGUAGE CENSORSHIP! 🚫', "embed": {
+				"title": "¡LANGUAGE CENSORSHIP!",
+				"description": '"' + messageSplit[i].replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").replace(/\s{2,}/g," ") + '" is a bad word. Don\'t use it. >:(',
+				"color": 0xff0000,
+				"footer": {
+					"text": "Glory to LEARAX57H"
+				},
+				"image": {
+					"url": "https://getadblock.com/images/adblock_logo_stripe_test.png"
+				}
+			}
+			});
+			break censorLoop;
+		};
+	};
 
-  if (message.content.includes('🚫 ¡LANGUAGE CENSORSHIP! 🚫') && message.author.username == 'Arco') {
-    delay(5000);
-    message.delete();
-  };
+	if (message.content.includes('🚫 ¡LANGUAGE CENSORSHIP! 🚫') && message.author.username == 'Arco') {
+		delay(5000);
+		message.delete();
+	};
 
-  if (message.author.username == 'Guzaboo') {
-    message.react('🤔');
-  };
+	if (message.author.username == 'Guzaboo') {
+		message.react('🤔');
+	};
 
-  if (!message.author.bot) {
+	if (!message.author.bot) {
 
-    // Start main processing
+		// Start main processing
 
-    // Calculator abilities
-    if (message.content.charAt(0) == '#') {
-      console.log('////MATH TIME////');
-      var mathInput = message.content.slice(1);
-      console.log('Input: ' + mathInput);
+		// Calculator abilities
+		if (message.content.charAt(0) == '#') {
+			console.log('////MATH TIME////');
+			var mathInput = message.content.slice(1);
+			console.log('Input: ' + mathInput);
 
-      try {
+			try {
 
-        var mathOutput = math.eval(mathInput);
+				var mathOutput = math.eval(mathInput);
 
-      } catch (error) {
-        console.log('Error Encountered: ' + error);
+			} catch (error) {
+				console.log('Error Encountered: ' + error);
 
-        try {
-          console.log('Trying mathsteps...');
-          let steps = mathsteps.simplifyExpression(mathInput);
-          steps.forEach(step => {
-            var mathOutput = step.newNode;
-            console.log('After mathsteps: ' + mathOutput);
-          });
+				try {
+					console.log('Trying mathsteps...');
+					let steps = mathsteps.simplifyExpression(mathInput);
+					steps.forEach(step => {
+						var mathOutput = step.newNode;
+						console.log('After mathsteps: ' + mathOutput);
+					});
 
-        } catch (error) {
+				} catch (error) {
 
-          console.log('Error ' + error + ' encountered. Failure.');
-          message.reply('Error encountered: ' + mathInput + ' invalid. Error: ' + error);
+					console.log('Error ' + error + ' encountered. Failure.');
+					message.reply('Error encountered: ' + mathInput + ' invalid. Error: ' + error);
 
-        };
-      };
+				};
+			};
 
-      if (mathOutput !== undefined) {
-        message.delete();
-        console.log('Math output valid.');
-        message.channel.send(mathInput + ' = ' + mathOutput);
-      };
+			if (mathOutput !== undefined) {
+				message.delete();
+				console.log('Math output valid.');
+				message.channel.send(mathInput + ' = ' + mathOutput);
+			};
 
-      // Admin command handler
-    } else if (message.author.id == config.admin && message.content.charAt(0) == '$') {
-      let adminCommand = message.content.slice(1).split(' ');
-      console.log('Admin Command Issued: ' + adminCommand);
+			// Admin command handler
+		} else if (message.author.id == config.admin && message.content.charAt(0) == '$') {
+			let adminCommand = message.content.slice(1).split(' ');
+			console.log('Admin Command Issued: ' + adminCommand);
 
-      switch (adminCommand[0]) {
-        case 'sweep':
-        switch (adminCommand[1]) {
+			switch (adminCommand[0]) {
+				case 'sweep':
+					switch (adminCommand[1]) {
 
-          case 'content':
-          var sweepTargetContent = adminCommand.splice(2).join(' ');
-          console.log('Sweeping chat for messages matching ' + sweepTargetContent + '...');
-          message.channel.fetchMessages({limit:100}).then(messages => {
-            let Victims = messages.filter(message => message.content.includes(sweepTargetContent));
+						case 'content':
+							var sweepTargetContent = adminCommand.splice(2).join(' ');
+							console.log('Sweeping chat for messages matching ' + sweepTargetContent + '...');
+							message.channel.fetchMessages({limit:100}).then(messages => {
+								let Victims = messages.filter(message => message.content.includes(sweepTargetContent));
 
-            message.channel.bulkDelete(Victims);
-          });
-          break;
+								message.channel.bulkDelete(Victims);
+							});
+							break;
 
-          case 'charAt':
-          console.log('Sweeping chat for messages with a \'' + adminCommand[3] + '\' character in the ' + adminCommand[2] + 'position...')
-          message.channel.fetchMessages({limit:100}).then(messages => {
-            let Victims = messages.filter(message => message.content.charAt(adminCommand[2]) == adminCommand[3]);
+						case 'charAt':
+							console.log('Sweeping chat for messages with a \'' + adminCommand[3] + '\' character in the ' + adminCommand[2] + 'position...')
+							message.channel.fetchMessages({limit:100}).then(messages => {
+								let Victims = messages.filter(message => message.content.charAt(adminCommand[2]) == adminCommand[3]);
 
-            message.channel.bulkDelete(Victims);
-          });
-          break;
+								message.channel.bulkDelete(Victims);
+							});
+							break;
 
-          case 'author':
-          var sweepTargetUser = adminCommand.splice(2).join(' ');
-          console.log('Sweeping chat for messages from ' + sweepTargetUser + '...');
-          message.channel.fetchMessages({limit:100}).then(messages => {
-            let Victims = messages.filter(message => message.author.username == sweepTargetUser);
+						case 'author':
+							var sweepTargetUser = adminCommand.splice(2).join(' ');
+							console.log('Sweeping chat for messages from ' + sweepTargetUser + '...');
+							message.channel.fetchMessages({limit:100}).then(messages => {
+								let Victims = messages.filter(message => message.author.username == sweepTargetUser);
 
-            message.channel.bulkDelete(Victims);
-          });
-          break;
+								message.channel.bulkDelete(Victims);
+							});
+							break;
 
-          default:
-          console.log('Sweeping chat for ' + adminCommand[1] + ' messages...');
-          message.channel.fetchMessages({limit:adminCommand[1]}).then(messages => {
-            let Victims = messages;
+						default:
+							if (adminCommand[1] != undefined) {
+								console.log('Sweeping chat for ' + adminCommand[1] + ' messages...');
+							} else {
+								console.log('Removing past 100 messages...')
+							}
+							message.channel.fetchMessages({limit:adminCommand[1]}).then(messages => {
+								let Victims = messages;
 
-            message.channel.bulkDelete(Victims);
-          });
-          break;
-        };
-      };
-      message.delete();
-    };
-  };
+								message.channel.bulkDelete(Victims);
+							});
+							break;
+					};
+			};
+			message.delete();
+		};
+	};
 });
 
 
 function delay(milliseconds) {
-  var start = new Date().getTime();
-  for (i = 0; i < 1e7; i++) {
-    if ((new Date().getTime() - start) > milliseconds) {
-      break;
-    };
-  };
+	var start = new Date().getTime();
+	for (i = 0; i < 1e7; i++) {
+		if ((new Date().getTime() - start) > milliseconds) {
+			break;
+		};
+	};
 }
 
 bot.login(token);
